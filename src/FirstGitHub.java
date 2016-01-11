8b936d72f4b04787f57a49835856c747a73b0a05
crazoy <crazoy@163.com>
2016-01-11 19:40:14 +0800

    change from branch bug001

--- src/FirstGitHub.java
+++ src/FirstGitHub.java
@@ -4,6 +4,7 @@ public class FirstGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("from git hub.");
+		System.out.println(" modified from branch bug001");
 	}
 
 }
